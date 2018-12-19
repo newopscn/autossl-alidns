1d7c854d064b377aff6acddbc83994a0bf9e40f6
wanglin <wanglin@dbca.cn>
2018-12-19 12:18:35 +0800

    add cron scripts

--- update_cert_cron.sh
+++ update_cert_cron.sh
@@ -1,18 +1,22 @@
-$ cat update_cert_cron.sh 
 #!/bin/bash
 # createed by wanglin at 20181208
+# run on every two month
 
 logfile="/tmp/update_cert_cron.log"
-echo $(date +%Y%m%d-%H%M%S) >> $logfile
+echo "$(date +%Y%m%d-%H%M%S) update cert" >> $logfile
 
-# request new cert
-docker run -it --rm --name alidns -e ALI_SECRET_ID=xxx -e ALI_SECRET_KEY=xxx -v /data/ssl/:/etc/letsencrypt/ hub.digi-sky.com/base/alidns:1.0.0 2>&1 >> $logfile
-
-if [ $? -eq 0 ];then
-    # generate config files with new cert
-    cd /home/docker/gitlab/harbor && ./prepare --with-clair 2>&1 >> $logfile
+# run job at workday
+if [ $(date +%u) -lt 5 ];then
+    # request new cert
+    # no -t with crontab
+    docker run --rm --name alidns -e ALI_SECRET_ID=xxx -e ALI_SECRET_KEY=xxx -v /data/ssl/:/etc/letsencrypt/ hub.digi-sky.com/base/alidns:1.0.0 >> $logfile 2>&1
     
-    # restart container
-    su - docker -c "cd /home/docker/gitlab/harbor && docker-compose stop" 2>&1 >> $logfile
-    su - docker -c "cd /home/docker/gitlab/harbor && docker-compose start" 2>&1 >> $logfile
+    if [ $? -eq 0 ];then
+        # restart container
+        echo "$(date +%Y%m%d-%H%M%S) update server" >> $logfile
+        docker stop gitlabce_web_1 >> $logfile 2>&1
+        docker start gitlabce_web_1 >> $logfile 2>&1
+    fi
+else
+    echo "$(date +%Y%m%d-%H%M%S) dont run job at workend" >> $logfile
 fi
